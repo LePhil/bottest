d301cb1cc285933a2af34be77ee21e1f24ed2866
Phil <philipp.christen@gmail.com>
2017-05-09 20:42:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is a TestBot Server');
+    res.send('This is a TestBot Server ' +
+             'link: <a href="https://www.facebook.com/">asd</a>');
 });
 
 // Facebook Webhook
